6cf540611446ea8c39d0969f6f354b7c06ee4892
Sean Wrag <64014293+SeanWrag@users.noreply.github.com>
2026-01-17 13:50:56 -0500

    Update FibonacciApp.java

--- FibonacciApp.java
+++ FibonacciApp.java
@@ -1,4 +1,10 @@
 public class FibonacciApp {
+    /**
+ * Returns the nth term in the Fibonacci sequence using recursion.
+ *
+ * @param n the term index (0 returns 0, 1 returns 1)
+ * @return the Fibonacci value at term n
+ */
 
     public static int fib(int n) {
         if (n <= 1) {
